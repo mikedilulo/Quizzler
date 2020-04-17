318715ef1d3fba25f78d9fc73fef7ea32dac2037
mikedilulo <mikedilulo2020@gmail.com>
2020-04-17 14:50:48 -0600

    First 11 questions of quiz

--- quizzler-app/ViewController.swift
+++ quizzler-app/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     let quizQuestions = [
         Question(q: "Rubberbands don't last longer when they are refrigerated", a: "False"),
         Question(q: "Peanuts have one ingredient that is used in dynamite", a: "True"),
-        Question(q: "In Greece, the National Anthem is 158 verses", a: "True")
+        Question(q: "In Greece, the National Anthem is 158 verses", a: "True"),
+        Question(q: "There are 293 ways to make change for a dollar", a: "True"),
+        Question(q: "The average secretaries right hand does 56% of the typing", a: "False"),
+        Question(q: "Sharks are not the only animal that can blink with both eyes", a: "False"),
+        Question(q: "Black bears are the best kind of bear", a: "True"),
+        Question(q: "There are less chickens than people in the world", a: "False"),
+        Question(q: "In Greece, the National Anthem is 158 verses", a: "True"),
+        Question(q: "New Jersey grows 2/3 of the world's eggplants", a: "True"),
+        Question(q: "The longest one syllabus word in the English language is 'screeched'", a: "True")
     ]
     
     var questionNumber = 0
